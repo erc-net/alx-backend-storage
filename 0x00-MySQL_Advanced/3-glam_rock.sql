e20635c5e11b29f55332b048b6758f3c66c07091
Eric <111587424+erc-net@users.noreply.github.com>
2023-07-12 20:24:14 +0200

    Update 3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,7 @@
--- Search bands with style Glam rock
--- Durantion current
-
-SELECT band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan 
-FROM metal_bands 
-WHERE style LIKE '%Glam rock%';
-
+-- Old school band
+-- lifespan by band and style
+SELECT band_name,
+	ifnull(split, YEAR(CURDATE())) - formed as lifespan
+FROM metal_bands
+WHERE style LIKE "%Glam rock%"
+ORDER BY lifespan DESC;
